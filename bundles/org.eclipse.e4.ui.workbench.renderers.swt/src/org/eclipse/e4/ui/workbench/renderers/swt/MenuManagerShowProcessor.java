aa45504412af74fe856c5d8f95f22024361ce328
Paul Webster <pwebster@ca.ibm.com>
2013-01-14 13:36:38 -0500

    Bug 366528 - [Compatibility] Implement IMenuService#populateContributionManager
    Fix the toolbar image test failure, and add a fix for looking up the
    context.

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerShowProcessor.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerShowProcessor.java
@@ -19,6 +19,7 @@ import org.eclipse.e4.ui.internal.workbench.swt.WorkbenchSWTActivator;
 import org.eclipse.e4.ui.model.application.ui.MContext;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenu;
 import org.eclipse.e4.ui.model.application.ui.menu.MPopupMenu;
+import org.eclipse.e4.ui.model.internal.ModelUtils;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.swt.factories.IRendererFactory;
 import org.eclipse.jface.action.IMenuListener2;
@@ -133,6 +134,10 @@ public class MenuManagerShowProcessor implements IMenuListener2 {
 		final IEclipseContext evalContext;
 		if (menuModel instanceof MContext) {
 			evalContext = ((MContext) menuModel).getContext();
+		} else if (menuModel.getTransientData().containsKey(
+				ModelUtils.CONTAINING_CONTEXT)) {
+			evalContext = (IEclipseContext) menuModel.getTransientData().get(
+					ModelUtils.CONTAINING_CONTEXT);
 		} else {
 			evalContext = modelService.getContainingContext(menuModel);
 		}
